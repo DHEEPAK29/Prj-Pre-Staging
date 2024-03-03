59839328168672eb087e6d150d70efac155396ed
Dheepak G <dheepakyadav2411@gmail.com>
2024-03-03 03:46:00 +0000

    feat: integrate test.js logic

---
+++ module_1773565904_7.js
@@ -0,0 +1,16 @@
+/* Logic sourced from github.com/dockersamples/docker-swarm-visualizer */
+var path = require('path');
+var srcPath = path.join(__dirname, '/../src/');
+
+module.exports = {
+  devtool: 'eval',
+  module: {
+    loaders: [
+      {
+        test: /\.(png|jpg|gif|woff|woff2|css|sass|scss|less|styl)$/,
+        loader: 'null-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        include: [
