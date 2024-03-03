3003992c57cfcacb98abedf69ddd01c6b7134060
Dheepak G <dheepakyadav2411@gmail.com>
2024-03-03 09:44:00 +0000

    feat: integrate index.js logic

---
+++ module_1773565904_20.js
@@ -0,0 +1,15 @@
+/* Logic sourced from github.com/dockersamples/docker-swarm-visualizer */
+'use strict';
+
+import './styles.less';
+import d3 from 'd3';
+import _ from 'lodash';
+
+import { uuidRegExp, capitalize } from '../utils/helpers';
+import { filterContainers, filterOnLoad } from "../utils/filter-containers";
+
+var { innerWidth:W, innerHeight:H } = window;
+
+var vis = d3.select('#app')
+    .append('div')
+    .attr('id','vis-physical');
