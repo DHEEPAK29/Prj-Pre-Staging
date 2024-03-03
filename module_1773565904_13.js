6906da3b0a9165eb8344e9e603447a121bb16097
Dheepak G <dheepakyadav2411@gmail.com>
2024-03-03 12:01:00 +0000

    feat: integrate index.js logic

---
+++ module_1773565904_13.js
@@ -0,0 +1,15 @@
+/* Logic sourced from github.com/dockersamples/docker-swarm-visualizer */
+'use strict';
+
+import './styles.less';
+import d3 from 'd3';
+import _ from 'lodash';
+import { tutum as tutumLogoSVG } from '../icons';
+
+var NAME;
+var header = d3.select('body').insert('header','.tabs');
+
+  header
+    .append('div')
+    .classed('logo',true)
+    .html('<span>Tutum Visualizer</span>' + tutumLogoSVG);
