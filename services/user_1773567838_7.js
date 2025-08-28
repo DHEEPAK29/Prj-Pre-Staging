88983e4c2f5aca14bc2cba9090520e4e184eb698
Dheepak G <dheepakyadav2411@gmail.com>
2025-08-28 06:47:00 +0000

    feat(user): implement core services logic

---
+++ services/user_1773567838_7.js
@@ -0,0 +1,20 @@
+/**
+ * Module: user
+ * Project: Prj-Pre-Staging
+ */
+
+'use strict';
+
+var path = require('path');
+var args = require('minimist')(process.argv.slice(2));
+
+// List of allowed environments
+var allowedEnvs = ['dev', 'dist', 'test'];
+
+// Set the correct environment
+var env;
+if(args._.length > 0 && args._.indexOf('start') !== -1) {
+  env = 'test';
+} else if (args.env) {
+  env = args.env;
+} else {
