47a2bd1664c3354afe82b2b201c7fe9bcd7bd357
Dheepak G <dheepakyadav2411@gmail.com>
2025-08-31 20:48:00 +0000

    feat(config): implement core utils logic

---
+++ utils/config_1773568666_11.js
@@ -0,0 +1,20 @@
+/**
+ * Module: config
+ * Project: Prj-Pre-Staging
+ */
+
+// ORDER MATTERS!
+import physicalVisualization from './vis-physical';
+import provider from './data-provider';
+import { hasClass, removeClass, addClass, uuidRegExp } from './utils/helpers';
+let { MS } = window;
+
+require('normalize.css');
+require('animate.css/animate.css');
+require('./main.less');
+
+function parseQuery(qstr) {
+  var query = {};
+  var a = qstr.substr(1).split('&');
+  for (var i = 0; i < a.length; i++) {
+      var b = a[i].split('=');
