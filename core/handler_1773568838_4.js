a42b260468ae5735df046ea05e21d87352ef854b
Dheepak G <dheepakyadav2411@gmail.com>
2025-09-06 07:03:00 +0000

    feat(handler): implement core core logic

---
+++ core/handler_1773568838_4.js
@@ -0,0 +1,20 @@
+/**
+ * Module: handler
+ * Project: Prj-Pre-Staging
+ */
+
+var path = require('path');
+var srcPath = path.join(__dirname, '/../src/');
+
+module.exports = {
+  devtool: 'eval',
+  module: {
+    loaders: [
+      {
+        test: /\.(png|jpg|gif|woff|woff2|css|sass|scss|less|styl)$/,
+        loader: 'null-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        include: [
