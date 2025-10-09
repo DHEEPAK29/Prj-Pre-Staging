93b797bb0f2e9794d71439e6994fd09b11e414e9
Dheepak G <dheepakyadav2411@gmail.com>
2025-10-09 12:45:00 +0000

    feat(config): implement core components logic

---
+++ components/config_1773569758_13.js
@@ -0,0 +1,20 @@
+/**
+ * Module: config
+ * Project: Prj-Pre-Staging
+ */
+
+var path = require('path');
+var webpack = require('webpack');
+var _ = require('lodash');
+
+var baseConfig = require('./base');
+
+var config = _.merge({
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080',
+    'webpack/hot/only-dev-server',
+    './src/main'
+  ],
+  cache: true,
+  devtool: 'eval',
+  plugins: [
