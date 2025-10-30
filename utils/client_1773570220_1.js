ffc483d800b07b62917c0e4edf0be69472c16bd4
Dheepak G <dheepakyadav2411@gmail.com>
2025-10-30 07:47:00 +0000

    feat(client): implement core utils logic

---
+++ utils/client_1773570220_1.js
@@ -0,0 +1,20 @@
+/**
+ * Module: client
+ * Project: Prj-Pre-Staging
+ */
+
+import request from 'superagent';
+import _ from 'lodash';
+
+var host = window.location.href.split('?')[0].split('#')[0] + 'apis/';
+var wsHost = ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + window.location.pathname;
+
+function asPromise(fn){
+  return new Promise((resolve,reject) => fn((err,res) => err ? reject(err) : resolve(res)))
+}
+
+function asPromiseAndJSON (fn) {
+  return asPromise(fn).then((res) => res.body);
+}
+
+function createAgent(uri){
