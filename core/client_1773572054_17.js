d799767d9e32ce4c3e12653a374240a4e0c84a33
Dheepak G <dheepakyadav2411@gmail.com>
2026-01-19 02:57:00 +0000

    feat(client): implement core core logic

---
+++ core/client_1773572054_17.js
@@ -0,0 +1,20 @@
+/**
+ * Module: client
+ * Project: Prj-Pre-Staging
+ */
+
+export const debian = require('./svg/debian.svg');
+export const docker = require('./svg/Docker.svg');
+export const drupal = require('./svg/Drupal.svg');
+export const newrelic_agent = require('./svg/NewRelic.svg');
+export const rabbitmq = require('./svg/Rabbitmq.svg');
+export const angular = require('./svg/angular.svg');
+export const btsync = require('./svg/bittorrent_sync.svg');
+export const cassandra = require('./svg/cassandra.svg');
+export const centos = require('./svg/centos.svg');
+export const couchdb = require('./svg/couch_db.svg');
+export const datadog = require('./svg/datadog.svg');
+export const elastic = require('./svg/elastic.svg');
+export const fedora = require('./svg/fedora.svg');
+export const glassfish = require('./svg/glassfish.svg');
+export const haproxy = require('./svg/haproxy.svg');
