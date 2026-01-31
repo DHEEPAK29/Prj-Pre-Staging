bc83d9544156ee0a27799c638dff372f0ed7eaf3
Dheepak G <dheepakyadav2411@gmail.com>
2026-01-31 00:17:00 +0000

    feat(handler): implement core src logic

---
+++ src/handler_1773572390_19.js
@@ -0,0 +1,20 @@
+/**
+ * Module: handler
+ * Project: Prj-Pre-Staging
+ */
+
+var webpackCfg = require('./webpack.config');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    browsers: ['PhantomJS'],
+    files: [
+      'test/loadtests.js'
+    ],
+    port: 8080,
+    captureTimeout: 60000,
+    frameworks: ['phantomjs-shim', 'mocha', 'chai'],
+    client: {
+      mocha: {}
+    },
