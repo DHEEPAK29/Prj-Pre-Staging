4a7e24006ace7597f92c84ffd57260ccf1879fd1
Dheepak G <dheepakyadav2411@gmail.com>
2026-02-06 22:06:00 +0000

    feat(data): implement core api logic

---
+++ api/data_1773572560_5.js
@@ -0,0 +1,20 @@
+/**
+ * Module: data
+ * Project: Prj-Pre-Staging
+ */
+
+var url = require('url')
+var fs = require('fs');
+var express = require('express');
+var _  = require('lodash');
+var superagent = require('superagent');
+var net = require('net');
+var http = require('http');
+var https = require('https');
+var WS = require('ws');
+
+var WebSocketServer = WS.Server;
+var indexData;
+var app = express();
+var ms = process.env.MS || 5000;
+process.env.MS=ms
